561a51f5d14071fdf0e9bc89ecb42e4a6ce7a3a9
raktron <rakshithranganath5@gmail.com>
2021-05-31 22:16:51 +0530

    add test thresholds

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
     testEnvironment: 'node',
     testPathIgnorePatterns: ['<rootDir>/node_modules/'],
     collectCoverageFrom: ["src/**/*.js"],
+    coverageThreshold:{
+        global:{
+            branches: 75,
+            functions: 75,
+            lines: 75,
+            statements: 75
+          },
+    },
     modulePathIgnorePatterns: ['mock.js']
 }
